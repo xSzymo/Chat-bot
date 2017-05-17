a1a52fca350e20c6c9d8328d8ea4bf42ffdcec8f
xSzymo <szymekssss@wp.pl>
2017-05-17 14:49:50 +0200

    fix a few bugs in loadAtStart

--- src/main/java/com/xszymo/hibernate/configuration/LoadAtStart.java
+++ src/main/java/com/xszymo/hibernate/configuration/LoadAtStart.java
@@ -1,22 +1,24 @@
 package com.xszymo.hibernate.configuration;
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
 import com.xszymo.hibernate.interfaces.AnswersMessageService;
 import com.xszymo.hibernate.interfaces.QuestionsMessageService;
 import com.xszymo.hibernate.interfaces.UserService;
@@ -25,7 +27,7 @@ import com.xszymo.hibernate.tables.Question;
 
 @Configuration
 public class LoadAtStart {
-	public static final String PATH_QUESTIONS = "/home/xszymo/Desktop/Programming/text1.txt";
+	public static final String PATH_QUESTIONS = "/home/xszymo/Desktop/Programming/text.txt";
 	public static final boolean readQuestionsAndAnswersFromTxt = true;
 
 	@Resource(name = "answerMessageService")
@@ -68,17 +70,26 @@ public class LoadAtStart {
 											a = true;
 											i = k + 1;
 										}
+						if(i >= chars.length)
+							i = chars.length-1;
+						
 						if (!a)
 							hey += chars[i];
 						else
 							hey1 += chars[i];
 					}
-					Question questuerinio = new Question(hey.substring(0, hey.length() - 1));
-					Answer answerino = new Answer(hey1);
+					Question questuerinio = new Question(hey.substring(0, hey.length()-1));
+					Answer answerino = null;
+					if(!hey1.equals(" ")) {
+					answerino = new Answer(hey1);
 					answerino.setQuestion(questuerinio);
 					questuerinio.getAnswers().add(answerino);
+					}
 					question.persist(questuerinio);
-					answer.persist(answerino);
+					if(!hey1.equals(" ")) {
+						answer.persist(answerino);
+					}
+				    
 					line = br.readLine();
 				}
 
