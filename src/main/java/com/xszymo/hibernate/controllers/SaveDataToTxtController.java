6c90e1e3b9047b7c39a0f8d432672f9674bc306b
xSzymo <szymekssss@wp.pl>
2017-05-17 12:44:16 +0200

    add save to txt file operation from controller

---
+++ src/main/java/com/xszymo/hibernate/controllers/SaveDataToTxtController.java
@@ -0,0 +1,43 @@
+package com.xszymo.hibernate.controllers;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xszymo.hibernate.configuration.LoadAtStart;
+import com.xszymo.hibernate.interfaces.AnswersMessageService;
+import com.xszymo.hibernate.interfaces.QuestionsMessageService;
+import com.xszymo.hibernate.interfaces.UserService;
+import com.xszymo.hibernate.tables.Answer;
+import com.xszymo.hibernate.tables.Question;
+
+@Controller
+public class SaveDataToTxtController {
+	public static final String PATH_QUESTIONS = LoadAtStart.PATH_QUESTIONS;
+	
+	@Resource(name = "answerMessageService")
+	AnswersMessageService answer;
+
+	@Resource(name = "questionMessageService")
+	QuestionsMessageService question;
+	
+	@RequestMapping("saveToTxtFile")
+	public void saveAllMessages() throws IOException {
+		String everything = "";
+		for(Question x : question.findAll()) {
+			for(Answer x1 : x.getAnswers()) {
+				everything += x.getMessage() + " -.|.- " + x1.getMessage() + "\n";
+			}
+		}
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(PATH_QUESTIONS));
+		writer.write(everything);
+		if (writer != null)
+			writer.close();
+	}
+}
--- src/main/java/com/xszymo/hibernate/controllers/Start.java
+++ src/main/java/com/xszymo/hibernate/controllers/Start.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class AppController {
+public class Start {
 	@RequestMapping
 	public String start() {
 		return "start";
