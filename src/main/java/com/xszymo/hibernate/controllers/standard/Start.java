a67f2c179549ec8fc6754f8bcf946abfa050e61c
Szymon Laciak <SZYMON.LACIAK@pb.com>
2017-06-30 08:53:00 +0200

    remove unused files, add main structure

--- src/main/java/com/xszymo/hibernate/controllers/standard/Start.java
+++ src/main/java/com/xszymo/hibernate/controllers/standard/Start.java
@@ -1,14 +1,34 @@
 package com.xszymo.hibernate.controllers.standard;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class Start {
-	@RequestMapping
-	public String start() {
-		return "chat";
-	}
+    @GetMapping
+    public String start() {
+        return "choose";
+    }
 
+    @GetMapping("bot")
+    public String start1() {
+        return "chatBot";
+    }
+
+    @GetMapping("all")
+    public String start2() {
+        return "allChat";
+    }
+
+    @GetMapping("rooms")
+    public String start3() {
+        return "chatRooms";
+    }
+
+    @GetMapping("user")
+    public String start4() {
+        return "userChat";
+    }
 }
